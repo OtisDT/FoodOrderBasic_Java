d1c032c0729806d4592570eb93f6f2c1f0fca03d
Đức Đạt <ducdat2k1.38@gmail.com>
2023-01-01 12:10:44 +0700

    Initial commit

---
+++ FoodOrder/app/src/main/java/com/example/foodorder/constant/Constant.java
@@ -0,0 +1,23 @@
+package com.example.foodorder.constant;
+
+public interface Constant {
+
+    String GENERIC_ERROR = "General error, please try again later";
+
+    String PAGE_FACEBOOK = "";
+    String LINK_FACEBOOK = "https://www.facebook.com/otisdt.38";
+    String LINK_YOUTUBE = "";
+    String PHONE_NUMBER = "+84904717823";
+    String GMAIL = "ducdat2k1.38@gmail.com";
+    String SKYPE_ID = "";
+    String ZALO_LINK = "https://zalo.me/0904717823";
+
+    String FIREBASE_URL = "https://foodorder-2740b-default-rtdb.firebaseio.com";
+
+    String CURRENCY = " 000 VNĐ";
+    int TYPE_PAYMENT_CASH = 1;
+    String PAYMENT_METHOD_CASH = "Tiền mặt";
+
+    // Key Intent
+    String KEY_INTENT_FOOD_OBJECT = "food_object";
+}
